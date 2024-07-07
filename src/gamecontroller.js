c417fded2de81561a99b9ec3eb6a92985af4f2b2
Muhammad Saqib Ilyas <msaqib@gmail.com>
2024-07-07 16:22:13 +0500

    Implemented the new game, pause, resume, quite functionalities.

--- src/gamecontroller.js
+++ src/gamecontroller.js
@@ -3,34 +3,41 @@ import { ScoreObserver } from './scoreobserver';
 export default class GameController {
     constructor(context) {
         if (!GameController.instance) {
-            this.game = new Game(context);
-            const scoreElement = document.getElementById('score')
-            this.scoreUI = new ScoreObserver(scoreElement)
-            this.game.board.addScoreObserver(this.scoreUI)
-            this.game.board.addGameoverObserver(this)
-            this.intervalId = null;
+            this.context = context
             GameController.instance = this;
-            this.gameSpeed = 1;
         }
         return GameController.instance;
+    }
+
+    newGame() {
+        this.game = new Game(this.context);
+        const scoreElement = document.getElementById('score')
+        this.scoreUI = new ScoreObserver(scoreElement)
+        this.game.board.addScoreObserver(this.scoreUI)
+        this.game.board.addGameoverObserver(this)
+        this.intervalId = null;
+        this.gameSpeed = 1;
+        this.paused = false
     }
 
     handleInput() {
         // Handle keyboard input
         document.onkeydown = ( event => {
-            if (event.key === "ArrowUp") {
-                this.game.rotateCurrentTetromino()
-            }
-            else if (event.key === "ArrowRight") {
-                this.game.moveCurrentTetrominoRight()
-            }
-            else if (event.key === "ArrowLeft") {
-                this.game.moveCurrentTetrominoLeft()
-            }
-            else if (event.key === "ArrowDown") {
-                this.game.moveCurrentTetrominoDown()
-            }
-        })
+            if (!this.paused) {
+                if (event.key === "ArrowUp") {
+                    this.game.rotateCurrentTetromino()
+                    }
+                    else if (event.key === "ArrowRight") {
+                        this.game.moveCurrentTetrominoRight()
+                    }
+                    else if (event.key === "ArrowLeft") {
+                        this.game.moveCurrentTetrominoLeft()
+                    }
+                    else if (event.key === "ArrowDown") {
+                        this.game.moveCurrentTetrominoDown()
+                    }
+                }
+            })
     }
 
     startLoop() {
@@ -41,11 +48,25 @@ export default class GameController {
         this.handleInput()
     }
 
+    pauseLoop() {
+        clearInterval(this.intervalId)
+        this.paused = true
+    }
+
+    resumeLoop() {
+        this.startLoop()
+        this.paused = false
+    }
+
     stopLoop() {
         clearInterval(this.intervalId);
     }
 
     end() {
         this.stopLoop()
+        this.paused = true
+        this.game.board.removeGameoverObserver(this)
+        this.game.board.removeScoreObserver(this.scoreUI)
+        this.context.reset()
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,5 +5,39 @@ canvas.width = 300;
 canvas.height = 600;
 const context = canvas.getContext('2d');
 
-const controller = new GameController(context)
-controller.startLoop()
+const start = document.getElementById('start')
+
+start.onclick = () => {
+    const controller = new GameController(context)
+    controller.newGame()
+    controller.startLoop()
+    
+    const pause = document.createElement('button')
+    pause.innerText = 'Pause'
+    const panel = document.getElementById('buttons')
+    panel.appendChild(pause)
+
+    const end = document.createElement('button')
+    end.innerText = 'Quit'
+    panel.appendChild(end)
+
+    let state = 'running'
+    pause.onclick = () => {
+        if (state === 'running') {
+            pause.innerText = 'Resume'
+            controller.pauseLoop()
+            state = 'paused'
+        }
+        else if (state === 'paused') {
+            pause.innerText= 'Pause'
+            controller.resumeLoop()
+            state = 'running'
+        }
+    }
+
+    end.onclick = () => {
+        controller.end()
+        pause.remove()
+        end.remove()
+    }
+}
